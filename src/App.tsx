82b8e6ccbbf389169b5ab1491058888f57982fab
Egor Miliukov <milukove@gmail.com>
2023-08-11 01:39:28 +0300

    add disconnect redirect

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
             <CircularProgress />
           </div>
         }
+        disconnectRedirectUrl="https://www.grindery.io/sign-out?sidebar_opened=1"
       >
         <AppContextProvider>
           <HomePage />
